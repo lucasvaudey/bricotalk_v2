b770a829ffbca0478df8dbdf35afe0615c1bf7bf
lucasvaudey <lucasvaudey@gmail.com>
2021-03-18 18:53:05 +0100

    CRUD ready

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,32 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  orm.getMigrator().up();
   //seems to not working properly
   //we need to generate our migrator and upping only through CLI
   //or using code to create a migrations each time we run the server
-  // const post = orm.em.create(Post, {
-  //   title: "my third post",
-  //   desc: "hello this is the desc",
-  // });
-  // await orm.em.persist(post).flush();
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 main().catch((err) => {
   console.log("The error is :" + err);
---
+++ server/src/resolvers/posts.ts
@@ -0,0 +1,65 @@
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext) {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Arg("desc") desc: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title, desc });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Arg("desc", () => String, { nullable: true }) desc: string,
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+    }
+    if (desc) {
+      post.desc = desc;
+    }
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, { id });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+}
